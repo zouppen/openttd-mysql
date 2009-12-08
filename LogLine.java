61ccc8d2d6818aba38844e8fd452ed47cb86fe94
Joel Lehtonen <joel.lehtonen@iki.fi>
2009-12-09 00:11:59 +0200

    Yritettiin päivämääräparseria, mutta vituix män.

--- LogLine.java
+++ LogLine.java
@@ -1,11 +1,17 @@
 import java.util.Date;
 import java.util.regex.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParsePosition;
 
 public class LogLine {
 
 	private static final String logEntryRegEx = "^([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"";
 	private static final int logEntryGroups = 9;
 	private static final Pattern logEntryPattern;
+
+	private static final DateFormat apacheFormat;
+	private static final DateFormat outputFormat;
 	
 	public String ip;
 	public Date date;
@@ -17,6 +23,8 @@ public class LogLine {
 
 	static {
 		logEntryPattern = Pattern.compile(logEntryRegEx);
+		apacheFormat = new SimpleDateFormat("dd/MMM/yyy:HH:mm:ss Z");
+		outputFormat = DateFormat.getInstance();
 	}
 
 	public LogLine(String line) throws Exception {
@@ -26,17 +34,26 @@ public class LogLine {
 			throw new Exception("syntax error.");
 		}
 
+		ParsePosition position = new ParsePosition(0);
+
 		this.ip = matcher.group(1);
-		// this.date = matcher.group(4);
+		this.date = apacheFormat.parse(matcher.group(4),position);
 		this.request = matcher.group(5);
 		this.response = matcher.group(6);
-		//this.bytes = matcher.group(7);
+		this.bytes = Integer.parseInt(matcher.group(7));
 		if (!matcher.group(8).equals("-"))
 			this.referer = matcher.group(8);
 		this.browser = matcher.group(9);
+
+		System.out.println(matcher.group(4));
+		System.out.println(position.toString());
+
 	}
 
 	public String engineerDebug() {
-		return "IP: "+ this.ip + "\nBrowser: " + this.browser;
+		return "IP: "+ this.ip +
+			"\nDate: " + this.date.toString() +
+			//"\nDate: " + outputFormat.format(this.date) +
+			"\nBrowser: " + this.browser;
 	}
 }
