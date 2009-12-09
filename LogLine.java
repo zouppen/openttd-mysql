27c38d74de5754797579196c2e831cc7e5bcf7f1
Joel Lehtonen <joell@Joel-Lehtonen-MacBook.local>
2009-12-10 00:03:03 +0200

    Sisennetään javamaisesti.

--- LogLine.java
+++ LogLine.java
@@ -7,52 +7,52 @@ import java.text.ParsePosition;
 
 public class LogLine {
 
-	private static final String logEntryRegEx = "^([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"";
-	private static final int logEntryGroups = 9;
-	private static final Pattern logEntryPattern;
+    private static final String logEntryRegEx = "^([\\d.]+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"";
+    private static final int logEntryGroups = 9;
+    private static final Pattern logEntryPattern;
 
-	private static final DateFormat apacheFormat;
-	private static final DateFormat outputFormat;
+    private static final DateFormat apacheFormat;
+    private static final DateFormat outputFormat;
 	
-	public String ip;
-	public Date date;
-	public String request;
-	public String response;
-	public int bytes;
-	public String referer;
-	public String browser;
+    public String ip;
+    public Date date;
+    public String request;
+    public String response;
+    public int bytes;
+    public String referer;
+    public String browser;
 
-	static {
-		logEntryPattern = Pattern.compile(logEntryRegEx);
-		apacheFormat = new SimpleDateFormat("dd/MMM/yyy:HH:mm:ss Z",
-						    Locale.ENGLISH);
-		outputFormat = DateFormat.getInstance();
-	}
+    static {
+	logEntryPattern = Pattern.compile(logEntryRegEx);
+	apacheFormat = new SimpleDateFormat("dd/MMM/yyy:HH:mm:ss Z",
+					    Locale.ENGLISH);
+	outputFormat = DateFormat.getInstance();
+    }
 
-	public LogLine(String line) throws Exception {
+    public LogLine(String line) throws Exception {
  
-		ParsePosition position = new ParsePosition(0);
+	ParsePosition position = new ParsePosition(0);
 
-		Matcher matcher = logEntryPattern.matcher(line);
-		if (!matcher.matches() || logEntryGroups != matcher.groupCount()) {
-			throw new Exception("syntax error.");
-		}
-
-		this.ip = matcher.group(1);
-		this.date = apacheFormat.parse(matcher.group(4),position);
-		if (position.getErrorIndex() != -1)
-			throw new Exception("syntax error in date format.");
-		this.request = matcher.group(5);
-		this.response = matcher.group(6);
-		this.bytes = Integer.parseInt(matcher.group(7));
-		if (!matcher.group(8).equals("-"))
-			this.referer = matcher.group(8);
-		this.browser = matcher.group(9);
+	Matcher matcher = logEntryPattern.matcher(line);
+	if (!matcher.matches() || logEntryGroups != matcher.groupCount()) {
+	    throw new Exception("syntax error.");
 	}
 
-	public String engineerDebug() {
-		return "IP: "+ this.ip +
-			"\nDate: " + outputFormat.format(this.date) +
-			"\nBrowser: " + this.browser;
-	}
+	this.ip = matcher.group(1);
+	this.date = apacheFormat.parse(matcher.group(4),position);
+	if (position.getErrorIndex() != -1)
+	    throw new Exception("syntax error in date format.");
+	this.request = matcher.group(5);
+	this.response = matcher.group(6);
+	this.bytes = Integer.parseInt(matcher.group(7));
+	if (!matcher.group(8).equals("-"))
+	    this.referer = matcher.group(8);
+	this.browser = matcher.group(9);
+    }
+
+    public String engineerDebug() {
+	return "IP: "+ this.ip +
+	    "\nDate: " + outputFormat.format(this.date) +
+	    "\nBrowser: " + this.browser;
+    }
 }
--- Splitter.java
+++ Splitter.java
@@ -6,21 +6,22 @@ import java.util.NoSuchElementException;
  * Apache-login parsija
  */
 public class Splitter {
-
-	public static void main(String args[]) throws Exception {
-		for (String filename: args) {
-			Scanner scanner = new Scanner(new File(filename), "UTF-8");
-			
-			try {
-				String line = scanner.nextLine();
-				LogLine entry = new LogLine(line);
-				System.out.println(entry.engineerDebug());
-				
-			} catch (NoSuchElementException foo) {
-				// Tiedosto kaiketi loppu, kaikki ok.
-			} finally {
-				scanner.close();
-			}
-		}
+    
+    public static void main(String args[]) throws Exception {
+	for (String filename: args) {
+	    Scanner scanner = new Scanner(new File(filename), "UTF-8");
+	    
+	    try {
+		
+		String line = scanner.nextLine();
+		LogLine entry = new LogLine(line);
+		System.out.println(entry.engineerDebug());
+		
+	    } catch (NoSuchElementException foo) {
+		// Tiedosto kaiketi loppu, kaikki ok.
+	    } finally {
+		scanner.close();
+	    }
 	}
+    }
 }
