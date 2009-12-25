5b4a96e2f2f347f11d4b71f386b10c45fb9915ab
Joel Lehtonen <joel.lehtonen@iki.fi>
2009-12-25 17:54:30 +0200

    Added company info table support.

--- CompanyParser.java
+++ CompanyParser.java
@@ -4,13 +4,17 @@ import java.math.BigInteger;
 
 public class CompanyParser implements LineParser {
 
-    private static final String sqlStart = "INSERT company (company_id,money,loan,value,trains,roadvs,planes,ships) values ";
+    private static final String sqlStart_stats = "INSERT company_stats (company_id,money,loan,value,trains,roadvs,planes,ships) values ";
+    private static final String sqlStart_main = "INSERT IGNORE company (company_id,colour,name,founded) values ";
 
-    private static final String matchingRegEx = "^#:(\\d+).*Money: (\\d+)  Loan: (\\d+)  Value: (\\d+)  \\(T:(\\d+), R:(\\d+), P:(\\d+), S:(\\d+)\\).*$";
-    private static final int matchingGroups = 8;
+    private static final String matchingRegEx = "^#:(\\d+)\\(([^\\)]*)\\) Company Name: '([^']*)'  Year Founded: (\\d+)  Money: (\\d+)  Loan: (\\d+)  Value: (\\d+)  \\(T:(\\d+), R:(\\d+), P:(\\d+), S:(\\d+)\\).*$";
+    private static final int matchingGroups = 11;
     private static final Pattern matchingPattern;
 
     public Integer company_id;
+    public String colour;
+    public String name;
+    public Integer founded;
     public BigInteger money;
     public BigInteger loan;
     public BigInteger value;
@@ -39,20 +43,36 @@ public class CompanyParser implements LineParser {
 	}
 	
 	this.company_id = new Integer(matcher.group(1));
-	this.money = new BigInteger(matcher.group(2));
-	this.loan = new BigInteger(matcher.group(3));
-	this.value = new BigInteger(matcher.group(4));
-	this.trains = new Integer(matcher.group(5));
-	this.roadvs = new Integer(matcher.group(6));
-	this.planes = new Integer(matcher.group(7));
-	this.ships = new Integer(matcher.group(8));
+	this.colour = matcher.group(2);
+	this.name = matcher.group(3);
+	this.founded = new Integer(matcher.group(4));
+	this.money = new BigInteger(matcher.group(5));
+	this.loan = new BigInteger(matcher.group(6));
+	this.value = new BigInteger(matcher.group(7));
+	this.trains = new Integer(matcher.group(8));
+	this.roadvs = new Integer(matcher.group(9));
+	this.planes = new Integer(matcher.group(10));
+	this.ships = new Integer(matcher.group(11));
 
 	return true; // Success.
     }
 
     public void appendSQL(SQLBuilder sql) {
 
-	sql.appendRaw(sqlStart);
+	// Company info, database drops this if it's unchanged
+	sql.appendRaw(sqlStart_main);
+	sql.appendRaw('(');
+	sql.appendNumber(company_id);
+	sql.appendRaw(',');
+	sql.appendString(colour);
+	sql.appendRaw(',');
+	sql.appendString(name);
+	sql.appendRaw(',');
+	sql.appendNumber(founded);
+	sql.appendRaw(");");
+	
+	// Company statistics
+	sql.appendRaw(sqlStart_stats);
 	sql.appendRaw('(');
 	sql.appendNumber(company_id);
 	sql.appendRaw(',');
